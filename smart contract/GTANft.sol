b75198f915c0661143d010dd978d75597d591246
root <root@DESKTOP-4FQK56Q.>
2025-06-04 20:33:54 +0200

    Обновлены все файлы

--- smart contract/GTANft.sol
+++ smart contract/GTANft.sol
@@ -2,6 +2,7 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.26;
 
+// import OpenZeppelin Contracts
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
